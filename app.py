18592f9f872e421a79787dd8c4fdda04c4088462
Abdulhadi446 <abdulhadijunaidahmedkhan@gmail.com>
2024-11-21 20:36:55 +0500

    few fixes
    few fixes

--- app.py
+++ app.py
@@ -1381,7 +1381,7 @@ def main():
         elif "google search" == command or "search" == command or "google" == command:
             speak("What do you want to search.")
             num = listen()
-            ans(num)
+            speak(ans(num))
 
         else:
             answer = get_response(command)
